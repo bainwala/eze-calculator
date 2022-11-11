e946e4ff52117a4defae3f24288e760476bf6975
bainwala <hbainwala@gmail.com>
2022-11-10 19:55:21 -0600

    Set up header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,14 @@ import "../Styles/Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>This is the header</h1>
+      <div className="header_contents">
+        <img
+          className="calc_logo"
+          alt=""
+          src="https://www.webii.net/blog/wp-content/uploads/2021/08/calculator-form-web-dev-illustration.png"
+        />
+        <h1>Calculate Ezely!</h1>
+      </div>
     </div>
   );
 };
